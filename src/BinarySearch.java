23a874e4dd62e32ed4a6e669d5ae99a822349f32
VuPham93 <62596412+VuPham93@users.noreply.github.com>
2020-05-13 12:16:41 +0700

    Initial commit

---
+++ src/BinarySearch.java
@@ -0,0 +1,27 @@
+public class BinarySearch {
+    static int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
+    static int binarySearch(int[] list, int key, int low, int high) {
+        if (low <= high) {
+            int mid = low + (high - low)/2;
+
+            if (list[mid] == key) {
+                return mid;
+            }
+            if (list[mid] > key) {
+                return binarySearch(list, key, low , mid - 1);
+            } else
+                return binarySearch(list, key, mid + 1, high );
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int size = list.length -1;
+        System.out.println(binarySearch(list, 2, 0, size));
+        System.out.println(binarySearch(list, 11, 0, size));
+        System.out.println(binarySearch(list, 79, 0, size));
+        System.out.println(binarySearch(list, 1, 0, size));
+        System.out.println(binarySearch(list, 5, 0, size));
+        System.out.println(binarySearch(list, 80, 0, size));
+    }
+}
